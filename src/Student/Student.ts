51f0b7de1436a6f52489bfac86755eb13121e162
Willian R. Sabião <willianjedai@gmail.com>
2021-06-02 10:58:37 -0300

    feat(Student): add age to student

--- src/Student/Student.ts
+++ src/Student/Student.ts
@@ -1,12 +1,15 @@
-import Name from "../Name/Name";
-import Cpf from "../utils/validateCpf/Cpf";
+import Age from '../Age/Age'
+import Name from '../Name/Name'
+import Cpf from '../utils/validateCpf/Cpf'
 
 export default class Student {
-  name: Name;
-  cpf: Cpf;
-  
-  constructor (name: string, cpf: string) {
+  name: Name
+  cpf: Cpf
+  age: Age
+
+  constructor(name: string, cpf: string, birthday: string) {
     this.name = new Name(name)
     this.cpf = new Cpf(cpf)
+    this.age = new Age(birthday)
   }
 }
